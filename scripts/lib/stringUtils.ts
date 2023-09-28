e6c135611e9a337cf7da81177c27b147a6d6fb89
Eric Arellano <14852634+Eric-Arellano@users.noreply.github.com>
2023-09-28 13:53:32 -0400

    Port lib/sphinx to repo
    Co-authored-by: Axel Hernández Ferrera <axelhzf@gmail.com>

---
+++ scripts/lib/stringUtils.ts
@@ -0,0 +1,38 @@
+// This code is a Qiskit project.
+//
+// (C) Copyright IBM 2023.
+//
+// This code is licensed under the Apache License, Version 2.0. You may
+// obtain a copy of this license in the LICENSE file in the root directory
+// of this source tree or at http://www.apache.org/licenses/LICENSE-2.0.
+//
+// Any modifications or derivative works of this code must retain this
+// copyright notice, and modified files need to carry a notice indicating
+// that they have been altered from the originals.
+
+import { last, split } from 'lodash';
+
+export function removePart(text: string, separator: string, matcher: string[]) {
+  return text
+    .split(separator)
+    .filter((part) => !matcher.includes(part))
+    .join(separator);
+}
+
+export function removePrefix(text: string, prefix: string) {
+  if (text.startsWith(prefix)) {
+    return text.substring(prefix.length);
+  }
+  return text;
+}
+
+export function removeSuffix(text: string, suffix: string) {
+  if (text.endsWith(suffix)) {
+    return text.substring(0, text.length - suffix.length);
+  }
+  return text;
+}
+
+export function getLastPartFromFullIdentifier(fullIdentifierName: string) {
+  return last(split(fullIdentifierName, '.'))!;
+}
