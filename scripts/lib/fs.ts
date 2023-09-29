e8724413bfa5d8885f4efa308ce44efe140440bf
Eric Arellano <14852634+Eric-Arellano@users.noreply.github.com>
2023-09-29 10:55:52 -0400

    Migrate API pipeline script from closed source (#64)
    Part 2 of https://github.com/Qiskit/documentation/issues/9.
    
    The final part will run this script to generate the API docs. There are
    some things I want to tweak in the script like that the
    qiskit-ibm-runtime site has changed URLs, but I wanted to avoid making
    changes to the script in this PR so that it simply copies them over.
    
    This PR simply copies over the API pipeline written by @axelhzf from
    closed source to open source. Its only modifications are adding
    copyright headers and removing unused code from utility files.
    
    https://github.com/Qiskit/documentation/issues/63 tracks some ideas to
    simplify this pipeline in follow ups, such as not needing GitHub.
    
    Co-authored-by: Axel Hernández Ferrera <axelhzf@gmail.com>

---
+++ scripts/lib/fs.ts
@@ -0,0 +1,28 @@
+// This code is a Qiskit project.
+//
+// (C) Copyright IBM 2023.
+//
+// This code is licensed under the Apache License, Version 2.0. You may
+// obtain a copy of this license in the LICENSE file in the root directory
+// of this source tree or at http://www.apache.org/licenses/LICENSE-2.0.
+//
+// Any modifications or derivative works of this code must retain this
+// copyright notice, and modified files need to carry a notice indicating
+// that they have been altered from the originals.
+
+import { stat } from 'fs/promises';
+import { normalize } from 'path';
+
+export function getRoot() {
+  return normalize(`${__dirname}/../../`);
+}
+
+export async function pathExists(path: string) {
+  try {
+    await stat(path);
+    return true;
+  } catch (err: any) {
+    if (err && err.code === 'ENOENT') return false;
+    throw err;
+  }
+}
