5e70749d292159dbf4af31da4a15706f70414a7d
Eric Arellano <14852634+Eric-Arellano@users.noreply.github.com>
2023-10-20 11:22:31 -0400

    Fix permalinks with API generation for Sphinx 7.2+ (#212)
    Fixes one of the issues in
    https://github.com/Qiskit/documentation/issues/67, that we weren't
    removing the permalink in headers with qiskit-ibm-runtime:
    
    ![screenshot_2023-10-20_at_11 17
    25___am_720](https://github.com/Qiskit/documentation/assets/14852634/4fde70dc-e8c5-424f-9d34-c71cba68501a)
    
    This change is because of
    https://github.com/sphinx-doc/sphinx/commit/7e9a2066c2f31eda53d075f5a8b20a7430ca0c61.
    Sphinx 7.2 changed the title from "Permalink" to "Link".

--- scripts/lib/sphinx/sphinxHtmlToMarkdown.ts
+++ scripts/lib/sphinx/sphinxHtmlToMarkdown.ts
@@ -73,6 +73,8 @@ export async function sphinxHtmlToMarkdown(options: {
   // remove permalink links
   $main.find('a[title="Permalink to this heading"]').remove();
   $main.find('a[title="Permalink to this definition"]').remove();
+  $main.find('a[title="Link to this heading"]').remove();
+  $main.find('a[title="Link to this definition"]').remove();
 
   // remove download source code
   $main.find('p > a.reference.download.internal').closest('p').remove();
