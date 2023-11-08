a275f961c1c395044ecbaa7ad8f8280d85ccdd7f
Arnau Casau <arnaucasau@gmail.com>
2023-11-08 18:34:27 +0100

    Refactor of the link checker

---
+++ scripts/lib/LinkChecker.test.ts
@@ -0,0 +1,164 @@
+// This code is a Qiskit project.
+//
+// (C) Copyright IBM 2023.
+//
+// This code is licensed under the Apache License, Version 2.0. You may
+// obtain a copy of this license in the LICENSE file in the root directory
+// of this source tree or at http://www.apache.org/licenses/LICENSE-2.0.
+//
+// Any modifications or derivative works of this code must retain this
+// copyright notice, and modified files need to carry a notice indicating
+// that they have been altered from the originals.
+
+import { describe, expect, test } from "@jest/globals";
+import { File, Link } from "./LinkChecker"
+
+describe("Test the constructor of Link", () => {
+    test("Initialize internal link without anchors", () => {
+        let testLink = new Link("/testpath","/testorigin.mdx");
+        const attributes = [
+            testLink.value,
+            testLink.anchor,
+            testLink.origin,
+            testLink.status,
+            testLink.isExternal,
+        ];
+        const correct_values = [
+            "/testpath",
+            "",
+            ["/testorigin.mdx"],
+            [],
+            false,
+        ]
+        expect(attributes).toEqual(correct_values);
+    });
+    test("Initialize internal link with anchors", () => {
+        let testLink = new Link("/testpath#testanchor","/testorigin.mdx");
+        const attributes = [
+            testLink.value,
+            testLink.anchor,
+            testLink.origin,
+            testLink.status,
+            testLink.isExternal,
+        ];
+        const correct_values = [
+            "/testpath",
+            "#testanchor",
+            ["/testorigin.mdx"],
+            [],
+            false,
+        ]
+        expect(attributes).toEqual(correct_values);
+    });
+    test("Initialize external link", () => {
+        let testLink = new Link("https://test.link.com","/testorigin.mdx");
+        const attributes = [
+            testLink.value,
+            testLink.anchor,
+            testLink.origin,
+            testLink.status,
+            testLink.isExternal,
+        ];
+        const correct_values = [
+            "https://test.link.com",
+            "",
+            ["/testorigin.mdx"],
+            [],
+            true,
+        ]
+        expect(attributes).toEqual(correct_values);
+    });
+});
+
+describe("Validate links", () => {
+    test("Validate existing internal links with absolute path", () => {
+        let testLink = new Link("/testpath","/testorigin.mdx");
+        let testFile = new File("docs/testpath.mdx");
+        let allFiles: File[] = [testFile];
+        testLink.checkLink(allFiles);
+        expect(testLink.status).toEqual([true]);
+    });
+    test("Validate non-existing internal links with absolute path", () => {
+        let testLink = new Link("/test-alternative-path","/testorigin.mdx");
+        let testFile = new File("docs/testpath.mdx");
+        let allFiles: File[] = [testFile];
+        testLink.checkLink(allFiles);
+        expect(testLink.status).toEqual([false]);
+    });
+    test("Validate existing internal links with relative path", () => {
+        let testLink = new Link("../testpath","docs/test/testorigin.mdx");
+        let testFile = new File("docs/testpath.mdx");
+        let allFiles: File[] = [testFile];
+        testLink.checkLink(allFiles);
+        expect(testLink.status).toEqual([true]);
+    });
+    test("Validate non-existing internal links with relative path", () => {
+        let testLink = new Link("../testpath","docs/testorigin.mdx");
+        let testFile = new File("docs/testpath.mdx");
+        let allFiles: File[] = [testFile];
+        testLink.checkLink(allFiles);
+        expect(testLink.status).toEqual([false]);
+    });
+    test("Validate existing internal links with absolute path and multiple origin files", () => {
+        let testLink = new Link("/testpath","docs/test/testorigin.mdx");
+        testLink.addOrigin("docs/test/test2/testorigin.mdx");
+        testLink.addOrigin("docs/test/test3/testorigin.mdx");
+        testLink.addOrigin("docs/test/test2/test4/testorigin.mdx");
+        let testFile1 = new File("docs/testpath.mdx");
+        let testFile2 = new File("docs/test/test2/testpath.mdx");
+        let allFiles: File[] = [testFile1, testFile2];
+        testLink.checkLink(allFiles);
+        expect(testLink.status).toEqual([true,true,true,true]);
+    });
+    test("Validate non-existing internal links with absolute path and multiple origin files", () => {
+        let testLink = new Link("/testpath","docs/test/testorigin.mdx");
+        testLink.addOrigin("docs/test/test2/testorigin.mdx");
+        testLink.addOrigin("docs/test/test3/testorigin.mdx");
+        testLink.addOrigin("docs/test/test2/test4/testorigin.mdx");
+        let testFile1 = new File("docs/test/testpath.mdx");
+        let testFile2 = new File("docs/test2/test3/testpath.mdx");
+        let allFiles: File[] = [testFile1, testFile2];
+        testLink.checkLink(allFiles);
+        expect(testLink.status).toEqual([false,false,false,false]);
+    });
+    test("Validate internal links with relative path and multiple origin files", () => {
+        let testLink = new Link("../testpath","docs/test/testorigin.mdx");
+        testLink.addOrigin("docs/test/test2/testorigin.mdx");
+        testLink.addOrigin("docs/test/test3/testorigin.mdx");
+        testLink.addOrigin("docs/test/test2/test4/testorigin.mdx");
+        let testFile1 = new File("docs/testpath.mdx");
+        let testFile2 = new File("docs/test/test2/testpath.mdx");
+        let allFiles: File[] = [testFile1, testFile2];
+        testLink.checkLink(allFiles);
+        expect(testLink.status).toEqual([true,false,false,true]);
+    });
+});
+
+describe("Generate the possible paths of a given link", () => {
+    test("Possible links for an internal link with a relative path", () => {
+        let testLink = new Link("../testFile","docs/test/test2/testorigin.mdx");
+        let possiblePaths = testLink.resolve("docs/test/test2/testorigin.mdx")
+        let extectedPaths = [
+            "docs/test/testFile.md",
+            "docs/test/testFile.mdx",
+            "docs/test/testFile.ipynb",
+            "docs/test/testFile/index.md",
+            "docs/test/testFile/index.mdx",
+            "docs/test/testFile/index.ipynb",
+        ]
+        expect(possiblePaths).toEqual(extectedPaths);
+    });
+    test("Possible links for an internal link with an absolute path", () => {
+        let testLink = new Link("/testFile","docs/test/test2/testorigin.mdx");
+        let possiblePaths = testLink.resolve("docs/test/test2/testorigin.mdx")
+        let extectedPaths = [
+            "docs/testFile.md",
+            "docs/testFile.mdx",
+            "docs/testFile.ipynb",
+            "docs/testFile/index.md",
+            "docs/testFile/index.mdx",
+            "docs/testFile/index.ipynb",
+        ]
+        expect(possiblePaths).toEqual(extectedPaths);
+    });
+});
---
+++ scripts/lib/LinkChecker.ts
@@ -0,0 +1,140 @@
+// This code is a Qiskit project.
+//
+// (C) Copyright IBM 2023.
+//
+// This code is licensed under the Apache License, Version 2.0. You may
+// obtain a copy of this license in the LICENSE file in the root directory
+// of this source tree or at http://www.apache.org/licenses/LICENSE-2.0.
+//
+// Any modifications or derivative works of this code must retain this
+// copyright notice, and modified files need to carry a notice indicating
+// that they have been altered from the originals.
+
+import path from "node:path";
+
+const DOCS_ROOT = "./docs";
+const CONTENT_FILE_EXTENSIONS = [".md", ".mdx", ".ipynb"];
+
+export class File {
+    readonly path: string;
+    anchors: string[];
+  
+    constructor(path: string){
+      this.path = path;
+      this.anchors = [];
+    }
+
+    setAnchors(anchors: string[]){
+      this.anchors = anchors;
+    }
+  }
+
+export class Link {
+    readonly value: string;
+    readonly anchor: string;
+    readonly origin: string[];
+    readonly status: boolean[];
+    readonly isExternal: boolean;
+  
+    constructor(linkString: string, origin: string) {
+      /*
+       * linkString: Link as it appears in source file
+       *     origin: Path to source file containing link
+       */
+  
+      const splitLink = linkString.split("#", 2);
+      this.value = splitLink[0];
+      this.anchor = splitLink.length > 1 ? `#${splitLink[1]}` : "";
+      this.origin = [origin];
+      this.status = [];
+      this.isExternal = linkString.startsWith("http");
+    }
+
+    addOrigin(origin: string){
+        this.origin.push(origin);
+    }
+  
+    resolve(origin: string): string[] {
+      /*
+      * Return list of possible paths link could resolve to
+      */
+      if (this.isExternal) {
+        return [this.value];
+      }
+      if (this.value === "") {
+        return [origin];
+      } // link is just anchor
+      if (this.value.startsWith("/images")) {
+        return [path.join("public/", this.value)];
+      }
+  
+      let baseFilePath;
+      if (this.value.startsWith("/")) {
+        // Path is relative to DOCS_ROOT
+        baseFilePath = path.join(DOCS_ROOT, this.value);
+      } else {
+        // Path is relative to origin file
+        baseFilePath = path.join(path.dirname(origin), this.value);
+      }
+      // Remove trailing '/' from path.join
+      baseFilePath = baseFilePath.replace(/\/$/gm, "");
+  
+      // File may have one of many extensions (.md, .ipynb etc.), and/or be
+      // directory with an index file (e.g. `docs/build` should resolve to
+      // `docs/build/index.mdx`). We return a list of possible filenames.
+      let possibleFilePaths = [];
+      for (let index of ["", "/index"]) {
+        for (let extension of CONTENT_FILE_EXTENSIONS) {
+          possibleFilePaths.push(baseFilePath + index + extension);
+        }
+      }
+      return possibleFilePaths;
+    }
+
+    checkExternalLink(){
+        // External link checking not supported yet
+        return true;
+    }
+
+    checkInternalLink(existingFiles: File[], origin: string){
+      /*
+      * True if link is in `existingFiles`, otherwise false
+      */
+      const possiblePaths = this.resolve(origin);
+      for (let filePath of possiblePaths) {
+          if (existingFiles.some(file => file.path == filePath)) {
+              return true;
+          }
+      }
+      return false;
+    }
+  
+    checkLink(existingFiles: File[]) {
+      /*
+      * Adds a boolean in the `status` attribute for every 
+      * origin of the link, true if the link is in `existingFiles` 
+      * or is a valid external link, otherwise false
+      */
+      if (this.isExternal) {
+        // External link
+        const result = this.checkExternalLink();
+        for(let i=0; i<this.origin.length; i++){
+            this.status.push(result);
+        }
+      }else{
+      if (this.value.startsWith("/")) {
+        // Internal link (Absolute Path)
+        const result = this.checkInternalLink(existingFiles, "");
+        for(let i=0; i<this.origin.length; i++){
+            this.status.push(result);
+        }
+      }else{
+        // Internal link (Relative Path)
+        for(let origin of this.origin){
+            const result = this.checkInternalLink(existingFiles, origin);
+            this.status.push(result);
+        }
+      }
+    }
+  }
+}
