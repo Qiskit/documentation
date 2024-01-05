b998cdc8b9c0569fd3c489d888495a35cdff804f
Eric Arellano <14852634+Eric-Arellano@users.noreply.github.com>
2024-01-05 13:10:51 -0500

    Add more tests for API generation script (#581)
    Part of https://github.com/Qiskit/documentation/issues/223. 
    
    One way to improve the maintainability of our API generation is to test
    more. It gives us confidence we aren't breaking things and it makes it
    more obvious how the function behaves.
    
    This PR also makes some light refactoring like renaming things to be
    more clear.
    
    ---------
    
    Co-authored-by: Arnau Casau <arnaucasau@gmail.com>
    Co-authored-by: Arnau Casau <47946624+arnaucasau@users.noreply.github.com>

---
+++ scripts/lib/sphinx/specialCaseResults.test.ts
@@ -0,0 +1,57 @@
+// This code is a Qiskit project.
+//
+// (C) Copyright IBM 2024.
+//
+// This code is licensed under the Apache License, Version 2.0. You may
+// obtain a copy of this license in the LICENSE file in the root directory
+// of this source tree or at http://www.apache.org/licenses/LICENSE-2.0.
+//
+// Any modifications or derivative works of this code must retain this
+// copyright notice, and modified files need to carry a notice indicating
+// that they have been altered from the originals.
+
+import { expect, test } from "@jest/globals";
+
+import {
+  specialCaseResults,
+  PROVIDER_INDEX_META,
+  RUNTIME_INDEX_META,
+} from "./specialCaseResults";
+import { SphinxToMdResultWithUrl } from "./SphinxToMdResult";
+
+test("specialCaseResults()", () => {
+  const results = [
+    { url: "/api/my-pkg/release_notes" },
+    { url: "/api/my-pkg/terra" },
+    {
+      url: "/api/my-pkg/ibm-runtime",
+      meta: {},
+    },
+    {
+      url: "/api/my-pkg/ibm-provider",
+      meta: {},
+    },
+    { url: "/api/my-pkg/other-file" },
+  ] as SphinxToMdResultWithUrl[];
+
+  specialCaseResults(results);
+  expect(results).toEqual([
+    {
+      url: "/api/my-pkg/release-notes",
+    },
+    {
+      url: "/api/my-pkg/index",
+    },
+    {
+      url: "/api/my-pkg/index",
+      meta: { hardcoded_frontmatter: RUNTIME_INDEX_META },
+    },
+    {
+      url: "/api/my-pkg/index",
+      meta: { hardcoded_frontmatter: PROVIDER_INDEX_META },
+    },
+    {
+      url: "/api/my-pkg/other-file",
+    },
+  ]);
+});
