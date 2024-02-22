5ffd62ae72d7dc1cad160c657acbaac101b9e1e4
Eric Arellano <14852634+Eric-Arellano@users.noreply.github.com>
2024-02-22 13:29:37 -0500

    Add edge case to tests

--- scripts/lib/api/updateLinks.test.ts
+++ scripts/lib/api/updateLinks.test.ts
@@ -191,6 +191,7 @@ describe("relativizeLink()", () => {
     "https://ibm.com",
     "https://qiskit.org/ecosystem/nature",
     "https://qiskit.org/documentation/index.html",
+    "https://docs.quantum.ibm.com",  // Note there is no `/` at the end.
   ])("ignore irrelevant links", (input) => {
     expect(relativizeLink({ url: input })).toBeUndefined();
   });
