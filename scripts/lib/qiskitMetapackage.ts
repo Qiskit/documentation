53d96eb13f0c54b5f2b7cd18f239eb9c47e37da6
Arnau Casau <47946624+arnaucasau@users.noreply.github.com>
2024-02-23 19:21:26 +0100

    Link checker: Qiskit legacy release notes in a single FileBatch (#881)
    The legacy release notes of qiskit were generated from qiskit 0.45 and
    we need to check them loading those docs. This PR groups together all
    the legacy release notes in a FileBatch to avoid loading the same docs
    multiple times.

--- scripts/lib/qiskitMetapackage.ts
+++ scripts/lib/qiskitMetapackage.ts
@@ -10,33 +10,6 @@
 // copyright notice, and modified files need to carry a notice indicating
 // that they have been altered from the originals.
 
-// Some Qiskit release notes don't have docs in the repo, and we need to change
-// their links to point to the next closest version available. This map stores
-// the next closest version with docs for some qiskit legacy versions.
-export const QISKIT_MISSING_VERSION_MAPPING = new Map([
-  [
-    "0.19",
-    [
-      "0.5",
-      "0.6",
-      "0.7",
-      "0.8",
-      "0.9",
-      "0.10",
-      "0.11",
-      "0.12",
-      "0.13",
-      "0.14",
-      "0.15",
-      "0.16",
-      "0.17",
-      "0.18",
-    ],
-  ],
-  ["0.24", ["0.20", "0.21", "0.22", "0.23"]],
-  ["0.35", ["0.34"]],
-]);
-
 const QISKIT_METAPACKAGE_TO_TERRA = new Map([
   ["0.44", "0.25"],
   ["0.43", "0.24"],
