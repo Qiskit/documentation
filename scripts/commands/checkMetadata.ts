d49c97df826aa5fc575991019be4bc76473e845c
Kevin J. Sung <kevinjefferysung@gmail.com>
2024-02-26 17:36:55 -0500

    enforce 50 <= description length <= 160 (#890)
    Fixes #327

--- scripts/commands/checkMetadata.ts
+++ scripts/commands/checkMetadata.ts
@@ -54,7 +54,11 @@ const readMetadata = async (filePath: string): Promise<Record<string, any>> => {
 };
 
 const isValidMetadata = (metadata: Record<string, any>): boolean =>
-  metadata.title && metadata.description;
+  metadata.title &&
+  metadata.description &&
+  metadata.title != metadata.description &&
+  metadata.description.length <= 160 &&
+  metadata.description.length >= 50;
 
 const main = async (): Promise<void> => {
   const args = readArgs();
@@ -106,7 +110,7 @@ function handleErrors(mdErrors: string[], notebookErrors: string[]): void {
       ---
 
       The title should be the page title: it's used for browser tabs and the top line of search results. The description should describe the page
-      in <160 characters, ideally using some keywords. The description is what
+      in at least 50 but no more than 160 characters, ideally using some keywords. The description is what
       shows up as the text in search results. See https://github.com/Qiskit/documentation/issues/131 for some tips.
 
       Please fix these files:\n\n${mdErrors.join("\n")}
@@ -116,7 +120,8 @@ function handleErrors(mdErrors: string[], notebookErrors: string[]): void {
     console.error(`
       Invalid Jupyter notebook metadata. Every .ipynb file needs to 
       set 'title' and 'description' in the file metadata. You need to
-      manually add this metadata. 
+      manually add this metadata. Furthermore, the length of the description
+      must be at least 50 but no more than 160 characters.
       
       For example, if using VSCode, open up the file with
       the "Open With..." option and then "Text Editor".
@@ -130,7 +135,7 @@ function handleErrors(mdErrors: string[], notebookErrors: string[]): void {
       Finally, add new keys in the "metadata" section for "title" and "description".
       The title should be the page title: it's used for browser tabs
       and the top line of search results. The description should describe the page
-      in <160 characters, ideally using some keywords. The description is what
+      in at least 50 but no more than 160 characters, ideally using some keywords. The description is what
       shows up as the text in search results. See 
       https://github.com/Qiskit/documentation/issues/131 for some tips.
 
