36a82536a6db3795b32e0b43bacf6240ade169be
Eric Arellano <14852634+Eric-Arellano@users.noreply.github.com>
2024-03-21 16:52:36 -0400

    Fix 0.21 links

--- scripts/lib/links/ignores.ts
+++ scripts/lib/links/ignores.ts
@@ -64,14 +64,16 @@ type FilesToIgnores = { [id: string]: string[] };
 const _QPY_IGNORES = ["#f1", "#f2", "#f3", "#id2", "#id4", "#id6", "#id8"];
 
 const _RUNTIME_OBJECT_INV = Object.fromEntries(
-  ["", "dev/", "0.16/", "0.17/", "0.18/", "0.19/", "0.20/"].map((vers) => [
-    `public/api/qiskit-ibm-runtime/${vers}objects.inv`,
-    [
-      `/api/qiskit-ibm-runtime/${vers}qiskit_ibm_runtime.RuntimeEncoder#key_separator`,
-      `/api/qiskit-ibm-runtime/${vers}index#next-steps`,
-      `/api/qiskit-ibm-runtime/${vers}index#qiskit-runtime-version-api-docs-preview`,
+  ["", "dev/", "0.16/", "0.17/", "0.18/", "0.19/", "0.20/", "0.21/"].map(
+    (vers) => [
+      `public/api/qiskit-ibm-runtime/${vers}objects.inv`,
+      [
+        `/api/qiskit-ibm-runtime/${vers}qiskit_ibm_runtime.RuntimeEncoder#key_separator`,
+        `/api/qiskit-ibm-runtime/${vers}index#next-steps`,
+        `/api/qiskit-ibm-runtime/${vers}index#qiskit-runtime-version-api-docs-preview`,
+      ],
     ],
-  ]),
+  ),
 );
 
 const FILES_TO_IGNORES__EXPECTED: FilesToIgnores = {};
