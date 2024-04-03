feb22f32a753a7bb84e840bc22566277559969fa
Arnau Casau <arnaucasau@gmail.com>
2024-04-03 21:44:03 +0200

    Remove the math block's indentation

---
+++ scripts/lib/api/removeMathBlocksIndentation.ts
@@ -0,0 +1,33 @@
+// This code is a Qiskit project.
+//
+// (C) Copyright IBM 2024.
+//
+// This code is licensed under the Apache License, Version 2.0. You may
+// obtain a copy of this license in the LICENSE file in the root directory
+// of this source tree or at http://www.apache.org/licenses/LICENSE-2.0.
+//
+// Any modifications or derivative works of this code must retain this
+// copyright notice, and modified files need to carry a notice indicating
+// that they have been altered from the originals.
+
+import { HtmlToMdResultWithUrl } from "./HtmlToMdResult";
+
+/**
+ * The math blocks (latex expressions between double dollars $$) can't be
+ * indented more than one time. After introducing the MDX components for
+ * classes, functions, and attributes, we can't guarantee only one indentation,
+ * and this function removes them all
+ */
+function removeMathBlocksIndentation(results: HtmlToMdResultWithUrl[]): void {
+  for (const result of results) {
+    // Captures all the lines in the math blocks, including the indentation
+    // before the $$ characters
+    const mathBlockRegex = /^[^#\r\n](\s*)\$\$([\s\S]*?)\$\$/gm;
+    const indentRegex = /^ +/gm;
+    result.markdown = result.markdown.replace(mathBlockRegex, function (match) {
+      return match.replace(indentRegex, "");
+    });
+  }
+}
+
+export default removeMathBlocksIndentation;
