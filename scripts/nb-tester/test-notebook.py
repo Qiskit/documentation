845ea38e4b6fa8b4de1ba138a027c9970e55ca85
Frank Harkins <frankharkins@hotmail.co.uk>
2024-04-11 12:31:48 +0100

    Add notebook to 'submit jobs' list

--- scripts/nb-tester/test-notebook.py
+++ scripts/nb-tester/test-notebook.py
@@ -32,7 +32,7 @@ NOTEBOOKS_EXCLUDE = [
 ]
 NOTEBOOKS_THAT_SUBMIT_JOBS = [
     "docs/start/hello-world.ipynb",
-    "docs/analyze/saving-and-retrieving.ipynb",
+    "docs/run/save-and-retrieve.ipynb",
 ]
 
 
