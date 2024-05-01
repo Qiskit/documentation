eb5124431913da8a50f16b852bbe441cfbc12eb0
Eric Arellano <14852634+Eric-Arellano@users.noreply.github.com>
2024-05-01 12:08:32 -0400

    Regenerate Runtime docs to use module reorganization (#1270)
    Applies the changes made in
    https://github.com/Qiskit/documentation/issues/1240.
    
    Also updates the dev docs more generally.
    
    Before:
    
    <img width="272" alt="Screenshot 2024-05-01 at 11 46 46 AM"
    src="https://github.com/Qiskit/documentation/assets/14852634/d3655237-7a1e-4d3a-bc51-f8465b520b84">
    
    After:
    
    <img width="271" alt="Screenshot 2024-05-01 at 11 48 22 AM"
    src="https://github.com/Qiskit/documentation/assets/14852634/748bfbbc-6610-47c8-8d2d-4826feb0e20f">

--- scripts/commands/checkInternalLinks.ts
+++ scripts/commands/checkInternalLinks.ts
@@ -104,9 +104,11 @@ const PROVIDER_GLOBS_TO_LOAD = [
 const RUNTIME_GLOBS_TO_LOAD = [
   "docs/api/qiskit/providers_models.mdx",
   "docs/api/qiskit-ibm-runtime/options.mdx",
+  "docs/run/index.mdx",
   "docs/run/max-execution-time.mdx",
   "docs/run/configure-error-mitigation.mdx",
   "docs/run/configure-runtime-compilation.mdx",
+  "docs/transpile/index.mdx",
 ];
 const QISKIT_GLOBS_TO_LOAD = [
   "docs/build/circuit-construction.ipynb",
