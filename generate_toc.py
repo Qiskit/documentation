1dd057e50335ff42626579b7d922e0a0a786f211
Eric Arellano <14852634+Eric-Arellano@users.noreply.github.com>
2024-05-23 16:28:31 -0400

    Make slug a kwarg

--- generate_toc.py
+++ generate_toc.py
@@ -22,10 +22,10 @@ from page_content import (
 @dataclass
 class Entry:
     title: str
-    slug: str | None = None
-    children: list[Entry] = field(default_factory=lambda: [])
-    external_url: str | None = None
-    extra_page_content: str | None = None
+    slug: str | None = field(default=None, kw_only=True)
+    children: list[Entry] = field(default_factory=lambda: [], kw_only=True)
+    external_url: str | None = field(default=None, kw_only=True)
+    extra_page_content: str | None = field(default=None, kw_only=True)
 
     def __post_init__(self) -> None:
         if self.slug and self.external_url:
@@ -77,69 +77,71 @@ def entries_as_markdown_list(entries: list[Entry], *, indent: None | str = None)
 # ------------------------------------------------------------------------------
 
 GET_STARTED_CHILDREN = [
-    Entry("Introduction to Qiskit", "index", extra_page_content=index_page_content()),
+    Entry(
+        "Introduction to Qiskit", slug="index", extra_page_content=index_page_content()
+    ),
     Entry(
         "Install",
         children=[
-            Entry("Install Qiskit", "install-qiskit"),
-            Entry("Set up an IBM Quantum channel", "setup-channel"),
+            Entry("Install Qiskit", slug="install-qiskit"),
+            Entry("Set up an IBM Quantum channel", slug="setup-channel"),
         ],
     ),
-    Entry("Hello world", "hello-world"),
+    Entry("Hello world", slug="hello-world"),
     Entry(
         "Advanced setup",
         children=[
-            Entry("Install the Qiskit SDK from source", "install-qiskit-source"),
-            Entry("Configure the Qiskit SDK locally", "configure-qiskit-local"),
+            Entry("Install the Qiskit SDK from source", slug="install-qiskit-source"),
+            Entry("Configure the Qiskit SDK locally", slug="configure-qiskit-local"),
         ],
     ),
-    Entry("Latest updates", "latest-updates"),
+    Entry("Latest updates", slug="latest-updates"),
 ]
 
 CIRCUIT_CONSTRUCTION = [
     Entry(
         "Build circuits with Qiskit",
         children=[
-            Entry("Circuit library", "circuit-library"),
-            Entry("Construct circuits", "circuit-construction"),
-            Entry("Visualize circuits", "circuit-visualization"),
+            Entry("Circuit library", slug="circuit-library"),
+            Entry("Construct circuits", slug="circuit-construction"),
+            Entry("Visualize circuits", slug="circuit-visualization"),
             Entry(
                 "Classical feedforward and control flow",
-                "classical-feedforward-and-control-flow",
+                slug="classical-feedforward-and-control-flow",
             ),
-            Entry("Synthesizing unitary operators", "unitary-synthesis"),
-            Entry("Bit-ordering in Qiskit", "bit-ordering"),
-            Entry("Save circuits to disk", "save-circuits"),
+            Entry("Synthesizing unitary operators", slug="unitary-synthesis"),
+            Entry("Bit-ordering in Qiskit", slug="bit-ordering"),
+            Entry("Save circuits to disk", slug="save-circuits"),
         ],
     ),
     Entry(
         "Build operators with Qiskit",
         children=[
-            Entry("Operators module overview", "operators-overview"),
+            Entry("Operators module overview", slug="operators-overview"),
             Entry(
                 "Specifying observables in the Pauli basis",
-                "specify-observables-pauli",
+                slug="specify-observables-pauli",
             ),
-            Entry("Operators visualization", "operators-visualization"),
+            Entry("Operators visualization", slug="operators-visualization"),
         ],
     ),
     Entry(
         "Other circuit building tools",
         children=[
-            Entry("Pulse schedules", "pulse"),
+            Entry("Pulse schedules", slug="pulse"),
             Entry(
                 "OpenQASM",
                 children=[
-                    Entry("Intro to OpenQASM", "introduction-to-qasm"),
+                    Entry("Intro to OpenQASM", slug="introduction-to-qasm"),
                     Entry(
                         "OpenQASM 2 and the Qiskit SDK",
-                        "interoperate-qiskit-qasm2",
+                        slug="interoperate-qiskit-qasm2",
                     ),
                     Entry(
                         "OpenQASM 3 and the Qiskit SDK",
-                        "interoperate-qiskit-qasm3",
+                        slug="interoperate-qiskit-qasm3",
                     ),
-                    Entry("OpenQASM 3 feature table", "qasm-feature-table"),
+                    Entry("OpenQASM 3 feature table", slug="qasm-feature-table"),
                     Entry(
                         "OpenQASM 3.x live specification",
                         external_url="https://openqasm.com/",
@@ -154,10 +156,10 @@ TRANSPILER = [
     Entry(
         "Get started with the Qiskit transpiler",
         children=[
-            Entry("Introduction to transpilation", "transpile"),
-            Entry("Transpiler stages", "transpiler-stages"),
-            Entry("Transpile with pass managers", "transpile-with-pass-managers"),
-            Entry("Transpile with pass managers", "transpile-with-pass-managers"),
+            Entry("Introduction to transpilation", slug="transpile"),
+            Entry("Transpiler stages", slug="transpiler-stages"),
+            Entry("Transpile with pass managers", slug="transpile-with-pass-managers"),
+            Entry("Transpile with pass managers", slug="transpile-with-pass-managers"),
         ],
     ),
     Entry(
@@ -165,11 +167,13 @@ TRANSPILER = [
         children=[
             Entry(
                 "Default settings and configuration options",
-                "defaults-and-configuration-options",
+                slug="defaults-and-configuration-options",
             ),
-            Entry("Set optimization level", "set-optimization"),
-            Entry("Commonly used parameters for transpilation", "common-parameters"),
-            Entry("Represent quantum computers", "representing-quantum-computers"),
+            Entry("Set optimization level", slug="set-optimization"),
+            Entry(
+                "Commonly used parameters for transpilation", slug="common-parameters"
+            ),
+            Entry("Represent quantum computers", slug="representing-quantum-computers"),
         ],
     ),
     Entry(
@@ -177,12 +181,12 @@ TRANSPILER = [
         children=[
             Entry(
                 "Create a pass manager for dynamical decoupling",
-                "dynamical-decoupling-pass-manager",
+                slug="dynamical-decoupling-pass-manager",
             ),
-            Entry("Write a custom transpiler pass", "custom-transpiler-pass"),
-            Entry("Transpile against custom backends", "custom-backend"),
-            Entry("Install and use transpiler plugins", "transpiler-plugins"),
-            Entry("Create a transpiler plugin", "create-a-transpiler-plugin"),
+            Entry("Write a custom transpiler pass", slug="custom-transpiler-pass"),
+            Entry("Transpile against custom backends", slug="custom-backend"),
+            Entry("Install and use transpiler plugins", slug="transpiler-plugins"),
+            Entry("Create a transpiler plugin", slug="create-a-transpiler-plugin"),
         ],
     ),
     Entry(
@@ -190,35 +194,38 @@ TRANSPILER = [
         children=[
             Entry(
                 "Transpile circuits remotely with the Qiskit transpiler service",
-                "qiskit-transpiler-service",
+                slug="qiskit-transpiler-service",
             ),
-            Entry("AI transpiler passes", "ai-transpiler-passes"),
+            Entry("AI transpiler passes", slug="ai-transpiler-passes"),
         ],
     ),
 ]
 
 SIMULATORS = [
-    Entry("Introduction to debugging tools", "verify"),
-    Entry("Exact simulation with Qiskit primitives", "simulate-with-qiskit-primitives"),
+    Entry("Introduction to debugging tools", slug="verify"),
+    Entry(
+        "Exact simulation with Qiskit primitives",
+        slug="simulate-with-qiskit-primitives",
+    ),
     Entry(
         "Exact and noisy simulation with Qiskit Aer primitives",
-        "simulate-with-qiskit-aer",
+        slug="simulate-with-qiskit-aer",
     ),
-    Entry("Qiskit Runtime local testing mode", "local-testing-mode"),
-    Entry("Build noise models", "building-noise-models"),
-    Entry("Plot quantum states", "plot-quantum-states"),
+    Entry("Qiskit Runtime local testing mode", slug="local-testing-mode"),
+    Entry("Build noise models", slug="building-noise-models"),
+    Entry("Plot quantum states", slug="plot-quantum-states"),
     Entry(
         "Efficient simulation of stabilizer circuits with Qiskit Aer primitives",
-        "stabilizer-circuit-simulation",
+        slug="stabilizer-circuit-simulation",
     ),
     Entry(
         "IBM Quantum cloud-based simulators",
         children=[
             Entry(
                 "Using IBM Quantum cloud-based simulators",
-                "using-ibm-quantum-simulators",
+                slug="using-ibm-quantum-simulators",
             ),
-            Entry("Available IBM Quantum simulators", "cloud-based-simulators"),
+            Entry("Available IBM Quantum simulators", slug="cloud-based-simulators"),
         ],
     ),
 ]
@@ -227,17 +234,21 @@ PRIMITIVES = [
     Entry(
         "Run with primitives",
         children=[
-            Entry("Introduction to primitives", "primitives"),
-            Entry("Get started with primitives", "primitives-get-started"),
-            Entry("Primitives examples", "primitives-examples"),
+            Entry("Introduction to primitives", slug="primitives"),
+            Entry("Get started with primitives", slug="primitives-get-started"),
+            Entry("Primitives examples", slug="primitives-examples"),
         ],
     ),
     Entry(
         "Configure runtime options",
         children=[
-            Entry("Configure runtiem compilation", "configure-runtime-compilation"),
-            Entry("Configure runtime error mitigation", "configure-error-mitigation"),
-            Entry("Advanced runtime options", "advanced-runtime-options"),
+            Entry(
+                "Configure runtiem compilation", slug="configure-runtime-compilation"
+            ),
+            Entry(
+                "Configure runtime error mitigation", slug="configure-error-mitigation"
+            ),
+            Entry("Advanced runtime options", slug="advanced-runtime-options"),
         ],
     ),
 ]
@@ -248,41 +259,41 @@ EXECUTION_MODES = [
         children=[
             Entry(
                 "Introduction to Qiskit Runtime execution modes",
-                "execution-modes-intro",
+                slug="execution-modes-intro",
             ),
-            Entry("Introduction to Qiskit Runtime sessions", "sessions"),
-            Entry("Run jobs in a session", "run-jobs-in-session"),
-            Entry("Run jobs in a batch", "run-jobs-batch"),
-            Entry("FAQs", "execution-modes-faq"),
+            Entry("Introduction to Qiskit Runtime sessions", slug="sessions"),
+            Entry("Run jobs in a session", slug="run-jobs-in-session"),
+            Entry("Run jobs in a batch", slug="run-jobs-batch"),
+            Entry("FAQs", slug="execution-modes-faq"),
         ],
     ),
     Entry(
         "Manage jobs",
         children=[
-            Entry("Monitor a job", "monitor-job"),
-            Entry("Estimate job run time", "estimate-job-run-time"),
-            Entry("Minimize job run time", "minimize-time"),
+            Entry("Monitor a job", slug="monitor-job"),
+            Entry("Estimate job run time", slug="estimate-job-run-time"),
+            Entry("Minimize job run time", slug="minimize-time"),
             Entry(
                 "Maximum execution time for a Qiskit Runtime job or session",
-                "max-execution-time",
+                slug="max-execution-time",
             ),
         ],
     ),
 ]
 
 SYSTEMS_CHILDREN = [
-    Entry("Processor types", "processor-types"),
-    Entry("System information", "system-information"),
-    Entry("Get backend information with Qiskit", "get-backend-information"),
-    Entry("Native gates and operations", "native-gates"),
-    Entry("Retired systems", "retired-systems"),
+    Entry("Processor types", slug="processor-types"),
+    Entry("System information", slug="system-information"),
+    Entry("Get backend information with Qiskit", slug="get-backend-information"),
+    Entry("Native gates and operations", slug="native-gates"),
+    Entry("Retired systems", slug="retired-systems"),
     Entry(
         "Hardware considerations and limitations for classical feedforward and control flow",
-        "dynamic-circuits-considerations",
+        slug="dynamic-circuits-considerations",
     ),
-    Entry("Instances", "instances"),
-    Entry("Fair-share scheduler", "fair-share-queue"),
-    Entry("Manage cost", "manage-cost"),
+    Entry("Instances", slug="instances"),
+    Entry("Fair-share scheduler", slug="fair-share-queue"),
+    Entry("Manage cost", slug="manage-cost"),
 ]
 
 OPTIMIZE_FOR_HARDWARE_CHILDREN = [
@@ -297,41 +308,41 @@ EXECUTE_ON_HARDWARE_CHILDREN = [
 ]
 
 POSTPROCESS_CHILDREN = [
-    Entry("Retrieve and save results", "save-jobs"),
-    Entry("Visualize results", "visualize-results"),
+    Entry("Retrieve and save results", slug="save-jobs"),
+    Entry("Visualize results", slug="visualize-results"),
 ]
 
 # Note: not used in Workflow.
-SERVERLESS = [Entry("Qiskit Serverless workloads", "qiskit-serverless")]
+SERVERLESS = [Entry("Qiskit Serverless workloads", slug="qiskit-serverless")]
 
 WORKFLOW_FOLDER_AS_INDEX_CHILDREN = [
     Entry(
         "Introduction to Qiskit Patterns",
-        "patterns-index",
+        slug="patterns-index",
         extra_page_content=patterns_index_content(),
     ),
     Entry(
         "Map problem to circuits",
-        "map-problem-to-circuits-index",
+        slug="map-problem-to-circuits-index",
         extra_page_content=map_content(entries_as_markdown_list(CIRCUIT_CONSTRUCTION)),
     ),
     Entry(
         "Optimize for hardware",
-        "optimize-for-hardware-index",
+        slug="optimize-for-hardware-index",
         extra_page_content=optimize_content(
             entries_as_markdown_list(OPTIMIZE_FOR_HARDWARE_CHILDREN)
         ),
     ),
     Entry(
         "Execute on hardware",
-        "execute-on-hardware-index",
+        slug="execute-on-hardware-index",
         extra_page_content=execute_index_content(
             entries_as_markdown_list(EXECUTE_ON_HARDWARE_CHILDREN)
         ),
     ),
     Entry(
         "Postprocess results",
-        "postprocess-results-index",
+        slug="postprocess-results-index",
         extra_page_content=postprocess_index_content(
             entries_as_markdown_list(POSTPROCESS_CHILDREN)
         ),
