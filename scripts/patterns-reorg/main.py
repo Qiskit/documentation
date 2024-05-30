d10408337d3a3b39f540065b44d64a00220d94c2
Eric Arellano <14852634+Eric-Arellano@users.noreply.github.com>
2024-05-30 09:15:31 -0400

    Add script to migrate to new `guides/` structure (#1294)
    This is the first step in
    https://github.com/Qiskit/documentation/issues/1297.
    
    We're going to be making last-minute changes for the docs reorg, such as
    finalizing URLs at the last minute. So, it's useful to have a program
    that can immediately regenerate the entire migration for us at any time.
    This program will eventually generate two commits:
    
    1. Move files to guides/
    2. Update non-API internal links, like `/run/my-page` to
    `/guides/my-page`
    
    Thanks to this script being checked in to version control beforehand, we
    will have less code to review in the final migration PR.
    
    This program is not going to be permanent. We decided in
    https://github.com/Qiskit/documentation/issues/1299 that we will not
    generate the Workflow index pages longterm, only in this initial PR.
    Instead, we'll add a validation check. So this program is only temporary
    while we work on the migration.
    
    ## Follow up improvements
    
    This PR is only the initial foundation of the migration program.
    Remaining tasks:
    
    * Finalize the page URLs
    * Finalize the page organization in the left ToC and Patterns Index
    pages
    * Finalize the content for the new pages
    * Delete the existing pages and save the change as a Git commit.
    * Get CI to be green when running the migration program, such as the
    link checker.
    * https://github.com/Qiskit/documentation/issues/1299
    * https://github.com/Qiskit/documentation/issues/1460
    
    ---------
    
    Co-authored-by: javabster <abby.s.mitchell@gmail.com>
    Co-authored-by: Arnau Casau <arnaucasau@gmail.com>

---
+++ scripts/patterns-reorg/main.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python3.11
+
+# This code is a Qiskit project.
+#
+# (C) Copyright IBM 2024.
+#
+# This code is licensed under the Apache License, Version 2.0. You may obtain a
+# copy of this license in the LICENSE file in the root directory of this source
+# tree or at http://www.apache.org/licenses/LICENSE-2.0.
+#
+# Any modifications or derivative works of this code must retain this copyright
+# notice, and modified files need to carry a notice indicating that they have
+# been altered from the originals.
+
+from __future__ import annotations
+
+import shutil
+import json
+from pathlib import Path
+
+from models import determine_redirects
+from entries import TOP_LEVEL_ENTRIES
+
+
+def write_guides_dir() -> None:
+    folder_path = Path("docs", "guides")
+    if folder_path.exists():
+        shutil.rmtree(folder_path)
+    folder_path.mkdir(parents=True)
+
+    for entry in TOP_LEVEL_ENTRIES:
+        entry.ensure_slugs_exist(folder_path)
+
+    result = {
+        "title": "Guides",
+        "collapsed": True,
+        "children": [entry.to_json("guides") for entry in TOP_LEVEL_ENTRIES],
+    }
+    text = json.dumps(result, indent=2)
+    (folder_path / "_toc.json").write_text(text)
+
+
+def write_redirects_file() -> None:
+    fp = Path("scripts/patterns-reorg/redirects.json")
+    redirects = determine_redirects(TOP_LEVEL_ENTRIES)
+    text = json.dumps(redirects, indent=2) + "\n"
+    fp.write_text(text)
+
+
+def main() -> None:
+    write_guides_dir()
+    write_redirects_file()
+
+
+if __name__ == "__main__":
+    main()
