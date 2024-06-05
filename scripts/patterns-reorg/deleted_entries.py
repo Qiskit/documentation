7e7e70624ab4ec217cf60493a96e03f321f63fa3
Arnau Casau <47946624+arnaucasau@users.noreply.github.com>
2024-06-05 19:50:04 +0200

    Update scripts/patterns-reorg/deleted_entries.py
    Co-authored-by: Eric Arellano <14852634+Eric-Arellano@users.noreply.github.com>

--- scripts/patterns-reorg/deleted_entries.py
+++ scripts/patterns-reorg/deleted_entries.py
@@ -1,7 +1,7 @@
 from models import DeletedPage
 
 DELETED_PAGES = (
-    DeletedPage(redirect_to="/map-problem-to-circuits", old_slug="build/index"),
-    DeletedPage(redirect_to="/execute-on-hardware", old_slug="run/index"),
-    DeletedPage(redirect_to="", old_slug="start/index"),
+    DeletedPage(old_slug="build/index", redirect_to="/map-problem-to-circuits"),
+    DeletedPage(old_slug="run/index", redirect_to="/execute-on-hardware"),
+    DeletedPage(old_slug="start/index", redirect_to=""),
 )
