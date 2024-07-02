bbddbdcd8cb95ce8d8530849d559e490d1a81afb
Eric Arellano <14852634+Eric-Arellano@users.noreply.github.com>
2024-07-02 13:29:43 -0400

    Remove patterns migration scripts

--- scripts/patterns-reorg/deleted_entries.py
+++
@@ -1,7 +0,0 @@
-from models import DeletedPage
-
-DELETED_PAGES = (
-    DeletedPage(old_slug="build/index", redirect_to="/map-problem-to-circuits"),
-    DeletedPage(old_slug="run/index", redirect_to="/execute-on-hardware"),
-    DeletedPage(old_slug="start/index", redirect_to=""),
-)
