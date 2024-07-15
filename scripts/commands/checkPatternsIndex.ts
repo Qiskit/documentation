845ed06ddd334c1b240934d533fe0b03edd13412
ABBY CROSS <across@us.ibm.com>
2024-07-15 17:59:22 -0400

    Revert "add to ignored urls (for now at least)"
    This reverts commit a5aa160fecbb0f4781d87fbd08a9097074839695.

--- scripts/commands/checkPatternsIndex.ts
+++ scripts/commands/checkPatternsIndex.ts
@@ -14,7 +14,7 @@ import { readFile } from "fs/promises";
 import { pathExists } from "../lib/fs";
 import type { TocEntry } from "../lib/api/generateToc";
 
-const IGNORED_URLS = ["/guides/first-serverless-program"];
+const IGNORED_URLS = ["/guides/qiskit-serverless"];
 
 const INDEX_PAGES = [
   "docs/guides/map-problem-to-circuits.mdx",
