092b22ae18dd3b6ed0aa2e407c6decc45113e76c
Rebecca Dimock <beckyd@us.ibm.com>
2024-08-27 15:43:40 -0500

    Ignore this page; it shouldn't be on an index page.

--- scripts/js/commands/checkPatternsIndex.ts
+++ scripts/js/commands/checkPatternsIndex.ts
@@ -14,7 +14,7 @@ import { readFile } from "fs/promises";
 
 import type { TocEntry } from "../lib/api/generateToc.js";
 
-const IGNORED_URLS = ["/guides/qiskit-serverless"];
+const IGNORED_URLS = ["/guides/qiskit-serverless", "/guides/qiskit-addons"];
 
 const INDEX_PAGES = [
   "docs/guides/map-problem-to-circuits.mdx",
