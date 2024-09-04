ab55a8067d48a212a10b6b5ba0d93418809dc063
Eric Arellano <14852634+Eric-Arellano@users.noreply.github.com>
2024-09-04 15:45:17 -0400

    Use ApiType in more places

--- scripts/js/lib/api/processHtml.ts
+++ scripts/js/lib/api/processHtml.ts
@@ -292,6 +292,10 @@ export async function processMembersAndSetMeta(
     const id = $dl.find("dt").attr("id") || "";
     const apiType = getApiType($dl);
 
+    if (apiType && apiType === "module") {
+      throw new Error("Did not expect apiType to be 'module'");
+    }
+
     const priorApiType = meta.apiType;
     if (!priorApiType) {
       meta.apiType = apiType;
@@ -315,7 +319,6 @@ export async function processMembersAndSetMeta(
     } else {
       const [openTag, closeTag] = await processMdxComponent(
         $,
-        $main,
         signatures,
         $dl,
         priorApiType,
@@ -399,6 +402,7 @@ function getApiType($dl: Cheerio<any>): ApiType | undefined {
     "property",
     "attribute",
     "module",
+    "data",
   ]) {
     if ($dl.hasClass(className)) {
       return className as ApiType;
