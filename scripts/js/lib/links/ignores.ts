453e7102b392978e7a1744f0179a09ab3355c092
Eric Arellano <14852634+Eric-Arellano@users.noreply.github.com>
2024-09-16 10:36:33 -0400

    Generate Qiskit 0.46.3 (#1952)
    This brings in a fix for `.. py:data::` in `utils.mdx`, so we can turn
    off those link checker ignores.

--- scripts/js/lib/links/ignores.ts
+++ scripts/js/lib/links/ignores.ts
@@ -124,7 +124,7 @@ function _qiskitUtilsData(): FilesToIgnores {
   // Qiskit docs used .. py:data:: incorrectly. We didn't fix these versions of the docs
   // because it is too tedious.
   const objectsInv = Object.fromEntries(
-    ["0.46/", "1.0/", "1.1/"].map((vers) => [
+    ["1.0/", "1.1/"].map((vers) => [
       `public/api/qiskit/${vers}objects.inv`,
       [
         `/api/qiskit/${vers}utils#qiskit.utils.optionals.HAS_AER`,
@@ -164,7 +164,7 @@ function _qiskitUtilsData(): FilesToIgnores {
     ]),
   );
   const utilsFile = Object.fromEntries(
-    ["0.46/", "1.0/", "1.1/"].map((vers) => [
+    ["1.0/", "1.1/"].map((vers) => [
       `docs/api/qiskit/${vers}utils.mdx`,
       [
         "#qiskit.utils.optionals.HAS_TESTTOOLS",
@@ -283,6 +283,9 @@ const FILES_TO_IGNORES__EXPECTED: FilesToIgnores = mergeFilesToIgnores(
     "docs/api/qiskit/0.46/qiskit.algorithms.optimizers.NFT.mdx": [
       "#id1",
       "#id2",
+    ],
+    "docs/api/qiskit/release-notes/0.46.mdx": [
+      "/api/qiskit-ibm-provider/ibm_jupyter",
     ],
   },
 );
