5d1f70e6d8cafa98f97d47a1766c0d78520ac567
Eric Arellano <14852634+Eric-Arellano@users.noreply.github.com>
2024-09-27 10:08:44 -0400

    Add missing anchor links to API docs (`div.section`) (#2043)
    Old versions of Sphinx would set `<div class="section">` with an `id`.
    These IDs are sometimes used by Sphinx cross-references for anchor
    links, so it's important to preserve them.

--- scripts/js/lib/api/htmlToMd.test.ts
+++ scripts/js/lib/api/htmlToMd.test.ts
@@ -280,6 +280,18 @@ test("preserve <span> with ids", async () => {
 `);
 });
 
+test("preserve ids of divs with class section", async () => {
+  expect(
+    await toMd(
+      `<div role="main"><div class="section" id="my-id"><p>Some text</p></div></div>`,
+    ),
+  ).toEqual(`<span id="my-id" />\n\nSome text\n`);
+  // The class `section` must be set.
+  expect(
+    await toMd(`<div role="main"><div id="my-id"><p>Some text</p></div></div>`),
+  ).toEqual(`Some text\n`);
+});
+
 test("merge contiguous <em> removing spaces", async () => {
   expect(
     await toMd(`
