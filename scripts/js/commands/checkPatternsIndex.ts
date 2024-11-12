8e335ea5a63d67b2a5da20fb9a656154961e7caa
Kaelyn Ferris <43348706+kaelynj@users.noreply.github.com>
2024-11-12 17:08:58 -0500

    Guides pages for OBP (#2248)
    Closes #2107 and closes #2108 
    
    I'll include the files in the toc and satiate CI once #2148 is merged to
    avoid dealing with conflicts.
    
    ---------
    
    Co-authored-by: Caleb Johnson <calebj1524@outlook.com>
    Co-authored-by: abbycross <across@us.ibm.com>

--- scripts/js/commands/checkPatternsIndex.ts
+++ scripts/js/commands/checkPatternsIndex.ts
@@ -22,6 +22,8 @@ const ALLOWLIST_MISSING_FROM_INDEX: Set<string> = new Set([
   "/guides/qiskit-code-assistant-vscode",
   "/guides/addons",
   "/guides/addons/qiskit-addons-sqd-get-started",
+  "/guides/qiskit-addons-obp",
+  "/guides/qiskit-addons-obp-get-started",
 ]);
 
 // URLs that show up in the INDEX_PAGES, but are not in the left ToC under
@@ -38,6 +40,8 @@ const ALLOWLIST_MISSING_FROM_TOC: Set<string> = new Set([
   "/guides/q-ctrl-optimization-solver",
   "/guides/qunasys-quri-chemistry",
   "/guides/circuit-library",
+  "/guides/qiskit-addons-obp",
+  "/guides/qiskit-addons-obp-get-started",
 ]);
 
 const INDEX_PAGES = [
