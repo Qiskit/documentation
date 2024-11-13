024d4b0323a5da9b6bf7f682a209ad79b685568f
Eric Arellano <14852634+Eric-Arellano@users.noreply.github.com>
2024-11-12 22:49:59 -0500

    Add qiskit-addon-aqc-tensor API docs (#2295)

--- scripts/js/lib/links/ignores.ts
+++ scripts/js/lib/links/ignores.ts
@@ -19,6 +19,7 @@ export const IGNORED_FILES = new Set([
   "public/api/qiskit-ibm-runtime/0.15/objects.inv",
   "public/api/qiskit-ibm-runtime/0.16/objects.inv",
   "public/api/qiskit-addon-cutting/objects.inv",
+  "public/api/qiskit-addon-aqc-tensor/objects.inv",
   "public/api/qiskit-addon-mpf/objects.inv",
   "public/api/qiskit-addon-obp/objects.inv",
   "public/api/qiskit-addon-sqd/objects.inv",
