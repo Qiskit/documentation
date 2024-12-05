6491d0b4c5568da3183e02c87c0a4ea5108584e8
Arnau Casau <47946624+arnaucasau@users.noreply.github.com>
2024-12-05 15:34:50 +0100

    Extend the images' checker to check API docs (#2426)
    This PR extends the images' checker introduced in
    https://github.com/Qiskit/documentation/pull/2349 to check the API docs
    when the new argument `--apis` is set.
    
    ---------
    
    Co-authored-by: Frank Harkins <frankharkins@hotmail.co.uk>

--- scripts/js/commands/checkImages.ts
+++ scripts/js/commands/checkImages.ts
@@ -11,11 +11,43 @@
 // that they have been altered from the originals.
 
 import { globby } from "globby";
+import yargs from "yargs/yargs";
+import { hideBin } from "yargs/helpers";
 import { collectInvalidImageErrors } from "../lib/markdownImages.js";
 import { readMarkdown } from "../lib/markdownReader.js";
 
+// Known APIs containing images without alt text that will be skipped
+// by the checker.
+const APIS_TO_IGNORE__SHOULD_FIX: string[] = [
+  "qiskit",
+  "qiskit-ibm-runtime",
+  "qiskit-addon-cutting",
+  "qiskit-addon-mpf",
+  "qiskit-addon-obp",
+  "qiskit-addon-utils",
+];
+
+interface Arguments {
+  [x: string]: unknown;
+  apis: boolean;
+}
+
+const readArgs = (): Arguments => {
+  return yargs(hideBin(process.argv))
+    .version(false)
+    .option("apis", {
+      type: "boolean",
+      default: false,
+      description:
+        "Check the images in the current and dev versions of the API docs have alt text.",
+    })
+    .parseSync();
+};
+
 async function main() {
-  const files = await globby(["docs/**/*.{ipynb,mdx}", "!docs/api/**/*.mdx"]);
+  const args = readArgs();
+
+  const files = await determineTocFiles(args);
   const fileErrors: string[] = [];
 
   for (const file of files) {
@@ -24,7 +56,7 @@ async function main() {
 
     if (imageErrors.size) {
       fileErrors.push(
-        `Error in file '${file}':\n\t- ${[...imageErrors].join("\n\t- ")}`,
+        `Error in file '${file}':\n\t- ${[...imageErrors].join("\n\t- ")}\n`,
       );
     }
   }
@@ -32,11 +64,29 @@ async function main() {
   if (fileErrors.length) {
     fileErrors.forEach((error) => console.log(error));
     console.error(
-      "\n💔 Some images have problems. See https://github.com/Qiskit/documentation#images for instructions.\n",
+      "💔 Some images have problems. See https://github.com/Qiskit/documentation#images for instructions.\n",
     );
     process.exit(1);
   }
   console.log("✅ All images are valid.\n");
+}
+
+async function determineTocFiles(args: Arguments): Promise<string[]> {
+  // We always skip historical versions to simplify the code and to have a faster script.
+  // Even though it is possible for someone to add a new image without alt text to a
+  // historical version that wasn't in the original release, that's very unlikely.
+  // If it happens, it would probably be a backport from latest or dev, and the linter in
+  // the API repo should catch it.
+  //
+  // If an image is missed by the API repo's linter, it will still have an alt text defined,
+  // although it won't be very useful.
+  const globs = [
+    "docs/**/*.{ipynb,mdx}",
+    args.apis ? "!docs/api/*/([0-9]*)/*.mdx" : "!docs/api/**/*.mdx",
+    ...APIS_TO_IGNORE__SHOULD_FIX.map((api) => `!docs/api/${api}/**/*.mdx`),
+  ];
+
+  return await globby(globs);
 }
 
 main().then(() => process.exit());
