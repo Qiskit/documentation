9618ec6aaa51c8bf008b776462db17fecfe8b378
Frank Harkins <frankharkins@hotmail.co.uk>
2025-01-22 10:44:08 +0000

    Update scripts/config/external-redirects.ts
    Co-authored-by: Eric Arellano <14852634+Eric-Arellano@users.noreply.github.com>

--- scripts/config/external-redirects.ts
+++ scripts/config/external-redirects.ts
@@ -11,8 +11,4 @@ export const externalRedirects = [
     old: "https://github.com/Qiskit-Extensions/circuit-knitting-toolbox/issues/108",
     new: "https://github.com/Qiskit/qiskit-addon-cutting/issues/108",
   },
-  {
-    old: "https://github.com/Qiskit-Partners/qiskit-ibm/issues/209",
-    new: "https://github.com/Qiskit/qiskit-ibm-provider/issues/209",
-  },
 ];
