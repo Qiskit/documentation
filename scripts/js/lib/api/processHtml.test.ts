a4d080c54c696a049483057d9240ee189d3aa477
Frank Harkins <frankharkins@hotmail.co.uk>
2025-01-23 13:39:40 +0000

    Add extra test case

--- scripts/js/lib/api/processHtml.test.ts
+++ scripts/js/lib/api/processHtml.test.ts
@@ -323,7 +323,7 @@ test("updateRedirectedExternalLinks()", () => {
   const doc = CheerioDoc.load(
     `<a href="https://ibm.com/old">https://ibm.com/old</a>
     <a href="https://ibm.com/old">https://ibm.com/old (v3)</a>
-    <a href="https://ibm.com/old-but-not-redirected"></a>
+    <a href="https://ibm.com/old-but-not-redirected">https://ibm.com/old-but-not-redirected (v3)</a>
     <a href="https://basename.example-website.com/old#anchor?and=query"></a>
     <p>https://ibm.com/old</p>`,
   );
@@ -331,7 +331,7 @@ test("updateRedirectedExternalLinks()", () => {
   doc.expectHtml(
     `<a href="https://ibm.com/new">https://ibm.com/new</a>
     <a href="https://ibm.com/new">https://ibm.com/new (v3)</a>
-    <a href="https://ibm.com/old-but-not-redirected"></a>
+    <a href="https://ibm.com/old-but-not-redirected">https://ibm.com/old-but-not-redirected (v3)</a>
     <a href="https://newbasename.example.com/new#newanchor?query=new"></a>
     <p>https://ibm.com/old</p>`,
   );
