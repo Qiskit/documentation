6934219cc9d87d067bb477486a98d7fd88e26ee0
Arnau Casau <arnaucasau@gmail.com>
2025-01-27 12:28:15 +0100

    update normalizeUrl comment

--- scripts/js/lib/api/updateLinks.ts
+++ scripts/js/lib/api/updateLinks.ts
@@ -62,8 +62,9 @@ export function normalizeUrl(
   kwargs: { kebabCaseAndShorten: boolean; pkgName: string },
 ): string {
   if (isAbsoluteUrl(url)) return url;
-  // We only want to normalize the API URLs to transform them to kebab-case
-  // Todo: update URLs from other APIs, when they all use kebab-case.
+  // Absolute URLs are already normalized, except those pointing to the same API docs.
+  // For those cases, we need to transform them to kebab-case.
+  // Todo: Transform URLs pointing to other APIs, when they all use kebab-case.
   if (url.startsWith("/") && !url.startsWith(`/api/${kwargs.pkgName}`))
     return url;
   url = transformSpecialCaseUrl(url);
