514e35ed34103dce7f1b4d3ad29c34f3eacac9ee
Arnau Casau I Playa <arnau.casau@ibm.com>
2025-02-06 17:55:49 +0100

    fix lint

--- scripts/js/commands/checkInternalLinks.ts
+++ scripts/js/commands/checkInternalLinks.ts
@@ -29,6 +29,7 @@ const SYNTHETIC_FILES: string[] = [
   "docs/api/runtime/tags/jobs.mdx",
   "docs/api/qiskit-transpiler-service-rest/index.mdx",
   "docs/api/runtime/tags/usage.mdx",
+  "docs/api/runtime/tags/sessions.mdx",
 ];
 
 interface Arguments {
