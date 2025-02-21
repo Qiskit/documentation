04f05436a8b8b45f50b331f5a30e240975a22591
Arnau Casau I Playa <arnau.casau@ibm.com>
2025-02-21 13:24:55 +0100

    fix lint

--- scripts/config/external-redirects.ts
+++ scripts/config/external-redirects.ts
@@ -7,6 +7,6 @@ export const externalRedirects = {
     "https://github.com/Qiskit/qiskit-addon-cutting/issues/108",
   "https://docs.quantum.ibm.com/api/qiskit-ibm-runtime/qiskit_ibm_runtime.IBMBackend#open_session":
     "https://docs.quantum.ibm.com/api/qiskit-ibm-runtime/0.34/qiskit_ibm_runtime.IBMBackend#open_session",
-    "https://docs.quantum.ibm.com/api/qiskit/transpiler_preset#qiskit.transpiler.preset_passmanagers.generate_preset_pass_manager":
-    "https://docs.quantum.ibm.com/api/qiskit/qiskit.transpiler.generate_preset_pass_manager#qiskit.transpiler.generate_preset_pass_manager"
+  "https://docs.quantum.ibm.com/api/qiskit/transpiler_preset#qiskit.transpiler.preset_passmanagers.generate_preset_pass_manager":
+    "https://docs.quantum.ibm.com/api/qiskit/qiskit.transpiler.generate_preset_pass_manager#qiskit.transpiler.generate_preset_pass_manager",
 };
