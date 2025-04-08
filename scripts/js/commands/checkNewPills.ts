6991d3025b83b8adc0629d1a463b3f6576323ad6
Kaelyn Ferris <43348706+kaelynj@users.noreply.github.com>
2025-04-08 09:33:01 -0400

    Apply suggestions from code review
    Co-authored-by: Eric Arellano <14852634+Eric-Arellano@users.noreply.github.com>

--- scripts/js/commands/checkNewPills.ts
+++ scripts/js/commands/checkNewPills.ts
@@ -1,6 +1,6 @@
 // This code is a Qiskit project.
 //
-// (C) Copyright IBM 2024.
+// (C) Copyright IBM 2025.
 //
 // This code is licensed under the Apache License, Version 2.0. You may
 // obtain a copy of this license in the LICENSE file in the root directory
@@ -35,9 +35,7 @@ async function main() {
   for (const tocFile of tocFiles) {
     const tocNewPills = await findNewPills(tocFile);
     const outdatedPills = await getOutdatedPills(tocNewPills);
-    if (outdatedPills.length > 0) {
-      allOutdatedPills.push(...outdatedPills);
-    }
+    allOutdatedPills.push(...outdatedPills);
   }
   if (allOutdatedPills.length > 0) {
     console.error(
@@ -67,14 +65,10 @@ async function getOutdatedPills(newPills: string[]): Promise<string[][]> {
 }
 
 function getDateDifference(pubDate: string): boolean {
-  const [year, month, day] = pubDate.split('-').map(Number);
-  const oldDate = new Date(year, month-1, day);
-  const dateDiff = Math.abs(TODAY.getTime() - oldDate.getTime() )
-  const diffDays = Math.ceil(dateDiff / (1000 * 3600 * 24));
-  if (diffDays > 14) {
-    return true;
-  }
-  return false;
+  const oldDate = new Date(pubDate);
+  const msPerDay = 1000 * 60 * 60 * 24;
+  const daysDiff = (TODAY - oldDate) / msPerDay;
+  return daysDiff > 14;
 }
 
 // Parse _toc.json to extract urls and dicts containing 'isNew' entries
