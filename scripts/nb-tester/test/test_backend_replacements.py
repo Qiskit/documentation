bff84d451f66ea233865297c066b3ffa12456929
Frank Harkins <frankharkins@hotmail.co.uk>
2025-06-18 10:21:09 +0100

    Rename function

--- scripts/nb-tester/test/test_backend_replacements.py
+++ scripts/nb-tester/test/test_backend_replacements.py
@@ -6,38 +6,39 @@ from qiskit_docs_notebook_tester.execute import (
     internal_backends_to_open,
 )
 
-def in_notebook(cell_source: str, outputs: list[dict] | None = None) -> NotebookNode:
+def create_notebook(cell_source: str, outputs: list[dict] | None = None) -> NotebookNode:
+    """Create a notebook with a single code cell"""
     return new_notebook(cells=[new_code_cell(
         source=cell_source,
         outputs=[new_output(**o) for o in outputs] if outputs else []
     )])
 
 def test_simple_open_to_internal():
-    nb = in_notebook("backend = service.backend(\"ibm_brisbane\")")
+    nb = create_notebook("backend = service.backend(\"ibm_brisbane\")")
     received = open_backends_to_internal(nb)
     assert len(received.cells) == 1
     assert received.cells[0].source == "backend = service.backend(\"alt_brisbane\")"
 
 def test_simple_internal_to_open():
-    nb = in_notebook("backend = service.backend(\"alt_brisbane\")")
+    nb = create_notebook("backend = service.backend(\"alt_brisbane\")")
     received = internal_backends_to_open(nb)
     assert len(received.cells) == 1
     assert received.cells[0].source == "backend = service.backend(\"ibm_brisbane\")"
 
 def test_open_to_internal_with_superstring():
-    nb = in_notebook("ibm_brisbane_coupling_map = CouplingMap(...)")
+    nb = create_notebook("ibm_brisbane_coupling_map = CouplingMap(...)")
     received = open_backends_to_internal(nb)
     assert len(received.cells) == 1
     assert received.cells[0].source == "ibm_brisbane_coupling_map = CouplingMap(...)"
 
 def test_internal_to_open_superstring():
-    nb = in_notebook("alt_brisbane_coupling_map = CouplingMap(...)")
+    nb = create_notebook("alt_brisbane_coupling_map = CouplingMap(...)")
     received = internal_backends_to_open(nb)
     assert len(received.cells) == 1
     assert received.cells[0].source == "alt_brisbane_coupling_map = CouplingMap(...)"
 
 def test_internal_to_open_stream_outputs():
-    nb = in_notebook(
+    nb = create_notebook(
         "print(\"Using backend: {backend.name}\")",
         outputs=[{
             "output_type": "stream",
@@ -52,7 +53,7 @@ def test_internal_to_open_stream_outputs():
     assert received.cells[0].outputs[0].text == "Using backend: ibm_brisbane"
 
 def test_internal_to_open_result_outputs():
-    nb = in_notebook(
+    nb = create_notebook(
         "print(\"Using backend: {backend.name}\")",
         outputs=[{
             "output_type": "execute_result",
