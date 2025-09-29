a3edfa261cd705d529030e3cade184e34194e672
Eric Arellano <14852634+Eric-Arellano@users.noreply.github.com>
2025-09-29 08:48:39 -0400

    Refactor how we determine release note page versions (#3984)
    Prework for part 2 of
    https://github.com/Qiskit/documentation/issues/2904.
    
    This PR removes a confusing part of our release note code:
    `ReleaseNotesConfig.separatePagesVersions` was not guaranteed to have
    every version it needed! Now, the expectation is that when creating
    `ReleaseNotesConfig`, you already have all the versions populated.
    
    This doesn't fully unblock
    https://github.com/Qiskit/documentation/issues/2904, but it makes the
    code less confusing.

--- scripts/js/lib/api/releaseNotes.ts
+++ scripts/js/lib/api/releaseNotes.ts
@@ -108,7 +108,6 @@ export async function maybeUpdateReleaseNotesFolder(
   ) {
     return;
   }
-  addNewReleaseNotes(pkg);
   await updateHistoricalTocFiles(pkg);
   console.log("Generating release-notes/index");
   const indexMarkdown = generateReleaseNotesIndex(pkg);
@@ -116,17 +115,27 @@ export async function maybeUpdateReleaseNotesFolder(
 }
 
 /**
- * Check for markdown files in `docs/api/<pkg-name>/release-notes/,
- * then sort them and return the list of versions.
+ * For packages with separate release note pages, determine all the versions.
+ *
+ * This works by reading from the file-system and also considering the current
+ * version being generated. The file-system is how we determine the versions that
+ * are not being actively generated.
+ *
+ * Returns versions in descending order.
  */
-export const findSeparateReleaseNotesVersions = async (
+export async function determineReleaseNotesSeparetePagesVersions(
   pkgName: string,
-): Promise<string[]> => {
-  return (await $`ls docs/api/${pkgName}/release-notes`.quiet()).stdout
-    .trim()
-    .split("\n")
-    .map((x) => parse(x).name)
-    .filter((x) => x.match(/^\d/)) // remove index
+  versionWithoutPatch: string,
+): Promise<string[]> {
+  const versions = new Set(
+    (await $`ls docs/api/${pkgName}/release-notes`.quiet()).stdout
+      .trim()
+      .split("\n")
+      .map((x) => parse(x).name)
+      .filter((x) => x.match(/^\d/)), // remove index
+  );
+  versions.add(versionWithoutPatch);
+  return Array.from(versions)
     .sort((a: string, b: string) => {
       const aParts = a.split(".").map((x) => Number(x));
       const bParts = b.split(".").map((x) => Number(x));
@@ -141,16 +150,6 @@ export const findSeparateReleaseNotesVersions = async (
       return 0;
     })
     .reverse();
-};
-
-function addNewReleaseNotes(pkg: Pkg): void {
-  if (
-    pkg.releaseNotesConfig.separatePagesVersions[0] === pkg.versionWithoutPatch
-  ) {
-    // Entries already includes most recent release notes
-    return;
-  }
-  pkg.releaseNotesConfig.separatePagesVersions.unshift(pkg.versionWithoutPatch);
 }
 
 export function generateReleaseNotesIndex(pkg: Pkg): string {
