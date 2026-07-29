0423532876e6ae30c7d696c090258292f69d746b
Kaelyn Ferris <43348706+kaelynj@users.noreply.github.com>
2026-07-29 13:53:29 -0400

    Add pauli-prop infra

--- scripts/js/lib/api/Pkg.ts
+++ scripts/js/lib/api/Pkg.ts
@@ -68,6 +68,7 @@ export class Pkg {
     "qiskit-addon-utils",
     "qiskit-addon-paulice",
     "qiskit-addon-pna",
+    "pauli-prop",
   ];
 
   static VALID_NAMES = [
@@ -222,6 +223,15 @@ export class Pkg {
         ...args,
         title: "Qiskit propagated noise absorption",
         githubSlug: "Qiskit/qiskit-addon-pna",
+        kebabCaseAndShortenUrls: true,
+        language: "Python",
+      });
+    }
+    if (name === "pauli-prop") {
+      return new Pkg({
+        ...args,
+        title: "Pauli propagation",
+        githubSlug: "Qiskit/pauli-prop",
         kebabCaseAndShortenUrls: true,
         language: "Python",
       });
